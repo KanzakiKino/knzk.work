995f23a65a97aaa3493825ac99e4793fcde46be1
KanzakiKino <kino@knzk.work>
2018-06-22 12:42:27 +0900

    [update] Added notifies.

---
+++ notifications.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = [
+    {
+        "date": "2018/06/21",
+        "content": "Added works page.",
+    },
+    {
+        "date": "2018/06/21",
+        "content": "MAINTENANCE HAS BEEN FINISHED",
+    },
+];
--- variables.js
+++ variables.js
@@ -6,6 +6,7 @@ module.exports = {
         description: "",
         author: "KanzakiKino",
     },
+    notifications: require( "./notifications.js" ),
     menu: [
         {
             name: "Home",
