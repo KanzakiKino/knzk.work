2815ec1f7e2e458dfda97dd7f53f23a8df1314ba
KanzakiKino <kino@knzk.work>
2018-06-23 17:37:46 +0900

    [update]

--- variables.js
+++ variables.js
@@ -11,26 +11,26 @@ module.exports = {
         {
             name: "Home",
             url : "/",
-            image: "/icon/home.png",
+            image: "/img/home.png",
         },
         {
             name: "Works",
             url : "/works/",
-            image: "/icon/works.png",
+            image: "/img/works.png",
         },
     ],
     sns: [
         {
             url: "https://twitter.com/knzk_work",
-            image: "/icon/twitter.png",
+            image: "/img/twitter.png",
         },
         {
             url: "https://github.com/KanzakiKino",
-            image: "/icon/github.png",
+            image: "/img/github.png",
         },
         {
             url: "https://www.youtube.com/channel/UCR_YxYbOl-SqVbXNzOLMn2w",
-            image: "/icon/youtube.png",
+            image: "/img/youtube.png",
         }
     ],
 };
